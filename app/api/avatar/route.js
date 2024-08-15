282c4fb4f61ee89495f8f86ded2191ecd70412f7
Rahul Rajput <newasram86@gmail.com>
2024-08-15 08:59:00 +0000

    fix text centering

--- app/api/avatar/route.js
+++ app/api/avatar/route.js
@@ -9,21 +9,21 @@ export async function GET(request) {
 
     const svg = `
     <svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100%" height="100%" fill="${bgColor}" />
-        <text
-            x="50"
-            y="50%"
-            text-anchor="middle"
-            dominant-baseline="central"
-            font-size="${fontSize}"
-            fill="${textColor}"
-            font-weight="600"
-            font-family="Arial"
-        >
-            ${text}
-        </text>
-    </svg>
-`;
+    <rect width="100%" height="100%" fill="${bgColor}" />
+    <text
+        x="50%"
+        y="50%"
+        text-anchor="middle"
+        dominant-baseline="middle"
+        font-size="${fontSize}"
+        fill="${textColor}"
+        font-weight="600"
+        font-family="Arial"
+    >
+        ${text}
+    </text>
+</svg>
+    `;
 
     return new Response(svg, {
         headers: {
